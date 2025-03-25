c485315b3297497a5098b584fd3fafdc77538f8d
huangqimin001 <huangqimin001@ke.com>
2025-03-25 16:03:15 +0800

    :art: Add  to  to limit build to 3.7+

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     packages=['pngquant'],
     py_modules=[],
     install_requires=['Pillow', 'puremagic', ],
-    requires_python='>=3.7, <4',
+    python_requires='>=3.7, <4',
 
     classifiers=[
         "License :: OSI Approved :: BSD License",
